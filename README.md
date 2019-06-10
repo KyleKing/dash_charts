8fd7f5b92258675c862236e97e74bfc93e53037d
Kyle King <kking@meso-scale.com>
2019-06-09 20:21:10 -0400

    Add: favicon's and minor tweaks for Windows

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Dash-HelloWorld/debug/assets-path
 
-Appears to be an issue with how the assets/ directory is loaded on Windows vs. NIX.
+Demo using the assets/ folder or a custom one on Windows and Mac/UNIX. Additionally can be built with pyInstaller.
 
 ```sh
 poetry install
@@ -13,9 +13,12 @@ poetry run python main.py
 
 # In main.py, try different `app` variables to test the path
 
-# Build with PyInstaller
-poetry run pyinstaller main.py --noupx --add-data ./styles/blue-bg.css:./styles --add-data styles/question.jpg:styles --onefile
+# FIXME: are styles/ and assets/ directories added to onefile output by PyInstaller automatically?
+poetry run pyinstaller main.py --noupx --onefile
 
-# FIXME: for onefile, the styles.css isn't loaded. But this works for --onedir. In both, the questions.jpg file loads
-
+# Build with PyInstaller (Windows)
+poetry run pyinstaller main.py --noupx --add-data styles;styles --onefile
+poetry run pyinstaller main.py --noupx --add-data styles/blue-bg.css;styles --add-data styles/question.jpg;styles --add-data styles/favicon.ico;styles --onefile
+# Build with PyInstaller (*NIX)
+poetry run pyinstaller main.py --noupx --add-data styles/blue-bg.css:styles --add-data styles/question.jpg:styles --onefile
 ```
--- main.py
+++ main.py
@@ -7,39 +7,47 @@ See docs: https://dash.plot.ly/dash-deployment-server/static-assets
 import dash
 import dash_html_components as html
 
+# Test two different image files based on the source directory
+imgNames= ['casey-horner-1584668-unsplash.jpg', 'question.jpg']
+
 # ======================================================================================================================
 
 # # Should import the relative ./assets/ directory automatically
 # app = dash.Dash(__name__)
 # expec = 'beige background and nice landscape image (Photo by Casey Horner on Unsplash)'
-# imgName = 'casey-horner-1584668-unsplash.jpg'
+# imgName, imgAlt = imgNames
 
 # Should import the styles/ dir
 from pathlib import Path
 app = dash.Dash(__name__, assets_folder=str(Path('styles')))
-expec = 'light blue background and image of question mark from Wikipedia (Selena Wilke [Public domain])'
-imgName = 'question.jpg'
-
+expec = """light blue background and image of question mark from Wikipedia (Selena Wilke [Public domain]).
+Favicon should a G from Google and not the official Plotly/Dash icon"""
+imgName, imgAlt = imgNames[::-1]
 
 # ======================================================================================================================
+
+# The favicon ison is lost with PyInstaller
+app._favicon = app.get_asset_url('favicon.ico')
 
 app.layout = html.Div([
     html.Div(
         className='app-content',
         children=[
             html.H1(
-                children='Test Import Local Assets Styles',
+                children='Dash App Testing',
             ),
-            html.P(
-                children='Expected Style: {}'.format(expec),
-            ),
-            # FIXME: this image is cached,by Dash...
+
+            html.P(children='Expected Style: {}'.format(expec)),
             html.Img(src=app.get_asset_url(imgName)),
+
+            # FIXME: this image is cached,by Dash...
+            html.P(children='Below image should not exist'),
+            html.Img(src=app.get_asset_url(imgAlt)),
         ],
     ),
 ])
 
 if __name__ == '__main__':
-    # For PyInstaller, debug must be False. Otherwise returns:
+    # FYI: For PyInstaller, debug must be False. Otherwise returns:
     #   "AttributeError: 'FrozenImporter' object has no attribute 'filename'"
     app.run_server(debug=False)
