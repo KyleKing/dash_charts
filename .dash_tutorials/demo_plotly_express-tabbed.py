a20794d7e2c3659e21f17bd6a6250c073830f12d
Kyle King <KyleKing@users.noreply.github.com>
2019-06-22 11:13:05 -0400

    Refac: use class to manage app variables

--- .dash_tutorials/demo_plotly_express-tabbed.py
+++ .dash_tutorials/demo_plotly_express-tabbed.py
@@ -31,7 +31,7 @@ def MinGraph(**kwargs):
 
 # ======================================================================================================================
 # Create classes to manage tabs state. Easy to scale up or down
-# >> Demo uses sample data. User should replace with data loaded from a static CSV file, TinyDB, SQLite, etc.
+# >> Demo uses sample data. User could replace with data loaded from a static CSV file, TinyDB, SQLite, etc.
 
 
 class TabBase:
@@ -73,7 +73,6 @@ class Tab2(TabBase):
     title = 'Iris Data'
     data = px.data.iris()
     funcMap = OrderedDict([
-        ('scatter', px.scatter),
         ('histogram', px.histogram),
         ('density_contour', px.density_contour),
         ('strip', px.strip),
@@ -89,7 +88,6 @@ class Tab3(TabBase):
     title = 'Gapminder Data'
     data = px.data.gapminder()
     funcMap = OrderedDict([
-        ('line', px.line),
         ('area', px.area),
     ])
     dims = ('x', 'y', 'color', 'line_group', 'facet_row', 'facet_col')
@@ -131,84 +129,111 @@ class Tab6(TabBase):
         ('colors.carto', px.colors.carto.swatches),
     ])
 
-
-TABS = [Tab1(), Tab2(), Tab3(), Tab4(), Tab5(), Tab6()]
-TAB_LOOKUP = {_tab.title: _tab for _tab in TABS}
-
 # ======================================================================================================================
-# Declare the layout and callbacks
-app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+# Create class for application to control manage variable scopes
 
-# Suppress callback verification because the tab content won't be rendered to check the graph callbacks
-app.config['suppress_callback_exceptions'] = True
 
-# Create layout, then individual tabs
-app.layout = html.Div([
-    html.H1('Dash/Plotly Express Data Exploration Demo'),
-    dcc.Tabs(id='tabs-select', value=TABS[0].title, children=[
-        dcc.Tab(label=_tab.title, value=_tab.title) for _tab in TABS
-    ], style={'margin-bottom': '15px'}),
-    html.Div(id='tabs-content'),
-])
+class PXDemoApp:
+    """Plotly Express Demo application."""
 
-TAB_MAP = {
-    _tab.title: html.Div([
-        html.Div([
-            html.P([
-                'Plot Type:',
-                dcc.Dropdown(
-                    id='{}-func'.format(_tab.title), options=_tab.funcOpts, value=_tab.funcOpts[0]['label']
+    def __init__(self):
+        """Initialize tabs and application."""
+        self.TABS = [Tab1(), Tab2(), Tab3(), Tab4(), Tab5(), Tab6()]
+        self.TAB_LOOKUP = {_tab.title: _tab for _tab in self.TABS}
+
+        self.app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+
+    def run(self, *, debug=True, **kwargs):
+        """Run the application passing any kwargs to dash."""
+        # Suppress callback verification since tab content won't be rendered when callbacks created
+        self.app.config['suppress_callback_exceptions'] = True
+
+        self.createLayout()
+        self._registerTabCB()
+        for tab in self.TABS:
+            self._registerChartCB(tab)
+
+        self.app.run_server(debug=debug, **kwargs)
+
+    def createLayout(self):
+        """Create layout."""
+        self.app.layout = html.Div([
+            html.H1('Dash/Plotly Express Data Exploration Demo'),
+            dcc.Tabs(id='tabs-select', value=self.TABS[0].title, children=[
+                dcc.Tab(label=_tab.title, value=_tab.title) for _tab in self.TABS
+            ], style={'margin-bottom': '15px'}),
+            html.Div(id='tabs-content'),
+        ])
+        # Configure each tab
+        self.__createTabMap()
+
+    def __createTabMap(self):
+        """Create TAB_MAP to store the tab individual tab layouts."""
+        self.TAB_MAP = {
+            tab.title: html.Div([
+                html.Div([
+                    html.P([
+                        'Plot Type:',
+                        dcc.Dropdown(
+                            id='{}-func'.format(tab.title), options=tab.funcOpts, value=tab.funcOpts[0]['label']
+                        ),
+                    ])
+                ] + [
+                    html.P([
+                        _d + ':', dcc.Dropdown(id='{}-{}'.format(tab.title, _d), options=tab.colOpts)
+                    ]) for _d in tab.dims
+                ] + [
+                    html.P([
+                        _d + ':',
+                        dcc.Dropdown(id='{}-{}'.format(tab.title, _d),
+                                     options=[ddOpts(_l, _l) for _l in _list]),
+                    ]) for _d, _list in tab.dimsDict.items()
+                ], style={'width': '25%', 'float': 'left'},
                 ),
+                MinGraph(id='{}-graph'.format(tab.title), style={'width': '75%', 'display': 'inline-block'}),
             ])
-        ] + [
-            html.P([
-                _d + ':', dcc.Dropdown(id='{}-{}'.format(_tab.title, _d), options=_tab.colOpts)
-            ]) for _d in _tab.dims
-        ] + [
-            html.P([
-                _d + ':', dcc.Dropdown(id='{}-{}'.format(_tab.title, _d), options=[ddOpts(_l, _l) for _l in _list])
-            ]) for _d, _list in _tab.dimsDict.items()
-        ], style={'width': '25%', 'float': 'left'},
-        ),
-        MinGraph(id='{}-graph'.format(_tab.title), style={'width': '75%', 'display': 'inline-block'}),
-    ])
-    for _tab in TABS
-}
+            for tab in self.TABS
+        }
 
+    def _registerTabCB(self):
+        """Register callback to handle tab rendering."""
+        @self.app.callback(
+            Output('tabs-content', 'children'),
+            [Input('tabs-select', 'value')],
+        )
+        def renderTabs(tabName):
+            """Render tabs when switched."""
+            return self.TAB_MAP[tabName]
 
-@app.callback(Output('tabs-content', 'children'), [Input('tabs-select', 'value')])
-def renderTabs(tabName):
-    """Render tabs when switched."""
-    return TAB_MAP[tabName]
+    def _registerChartCB(self, tab):
+        """Register the callbacks for handling the user input panel and chart rendering.
 
+        tab -- tab dictionary.
 
-# Register callbacks for each tab
-for _tab in TABS:
-    inputs = [Input('tabs-select', 'value'), Input('{}-func'.format(_tab.title), 'value')]
-    inputs.extend([Input('{}-{}'.format(_tab.title, _d), 'value') for _d in _tab.dims])
-    inputs.extend([Input('{}-{}'.format(_tab.title, _k), 'value') for _k in _tab.dimsDict.keys()])
+        """
+        inputs = [Input('tabs-select', 'value'), Input('{}-func'.format(tab.title), 'value')]
+        inputs.extend([Input('{}-{}'.format(tab.title, _d), 'value') for _d in tab.dims])
+        inputs.extend([Input('{}-{}'.format(tab.title, _k), 'value') for _k in tab.dimsDict.keys()])
 
-    @app.callback(
-        Output('{}-graph'.format(_tab.title), 'figure'),
-        inputs,
-    )
-    def renderFigure(tabName, nameFunc, *args):
-        """Create the figure."""
-        # Check if the trigger event is a tab change. If so, return an empty chart
-        triggeredIDs = [_t['prop_id'] for _t in dash.callback_context.triggered]
-        ic(triggeredIDs)
-        if 'tabs-select.value' in triggeredIDs:
-            return {}
-        # Otherwise, parse the arguments to generate a new plot
-        tab = TAB_LOOKUP[tabName]
-        if tab.takesArgs:
-            keys = list(tab.dims) + list(tab.dimsDict.keys())
-            kwargs = OrderedDict(zip(keys, args))
-            ic(kwargs)
-            ic(tab.data.head(2))
-            return tab.funcMap[nameFunc](tab.data, height=650, **kwargs)
-        return tab.funcMap[nameFunc]()
+        # Register callbacks for each tab
+        @self.app.callback(
+            Output('{}-graph'.format(tab.title), 'figure'),
+            inputs,
+        )
+        def renderFigure(tabName, nameFunc, *args):
+            """Create the figure."""
+            # Check if the trigger event is a tab change. If so, return an empty chart
+            ctx = dash.callback_context
+            if 'tabs-select.value' in [_t['prop_id'] for _t in ctx.triggered]:
+                return {}
+            # Otherwise, parse the arguments to generate a new plot
+            _tab = self.TAB_LOOKUP[tabName]
+            if tab.takesArgs:
+                keys = list(_tab.dims) + list(_tab.dimsDict.keys())
+                kwargs = OrderedDict(zip(keys, args))
+                return _tab.funcMap[nameFunc](_tab.data, height=650, **kwargs)
+            return _tab.funcMap[nameFunc]()
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    PXDemoApp().run(debug=True)
