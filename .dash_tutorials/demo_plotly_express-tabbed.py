46243e9103cb290337013598799f2c82ca2ec26b
Kyle King <KyleKing@users.noreply.github.com>
2019-06-22 10:37:47 -0400

    Add: context and polar/ternary charts

--- .dash_tutorials/demo_plotly_express-tabbed.py
+++ .dash_tutorials/demo_plotly_express-tabbed.py
@@ -25,18 +25,8 @@ def ddOpts(lbl, val):
 
 
 def MinGraph(**kwargs):
-    """Return dcc.Graph element with Plotly overlay removed.
-
-    See: https://community.plot.ly/t/is-it-possible-to-hide-the-floating-toolbar/4911/7
-
-    """
-    return dcc.Graph(
-        config={
-            'displayModeBar': False,
-            # 'modeBarButtonsToRemove': ['sendDataToCloud'],
-        },
-        **kwargs,
-    )
+    """Return dcc.Graph element with Plotly overlay removed."""
+    return dcc.Graph(config={'displayModeBar': False}, **kwargs)
 
 
 # ======================================================================================================================
@@ -52,6 +42,7 @@ class TabBase:
     data = pd.DataFrame()
     funcMap = OrderedDict([])
     dims = tuple()
+    dimsDict = OrderedDict([])
 
     def __init__(self):
         """Resolve higher-order data members."""
@@ -61,23 +52,45 @@ class TabBase:
 
 class Tab1(TabBase):
     """Tab1 properties."""
-    title = 'Scatter-Style'
-    data = px.data.tips()
+    title = 'Scatter Plot'
+    data = px.data.iris()
     funcMap = OrderedDict([
         ('scatter', px.scatter),
-        ('histogram', px.histogram),
-        ('line', px.line),
-        ('area', px.area),
-        ('density_contour', px.density_contour),
-        ('strip', px.strip),
-        ('box', px.box),
-        ('violin', px.violin),
     ])
-    dims = ('x', 'y', 'color', 'size', 'facet_col', 'facet_row')
+    dims = ('x', 'y', 'color', 'size', 'facet_row', 'facet_col')
+    dimsDict = OrderedDict([
+        ('marginal_x', ('histogram', 'rag', 'violin', 'box')),
+        ('marginal_y', ('histogram', 'rag', 'violin', 'box')),
+        ('trendline', ('ols', 'lowess')),
+        # Consider: 'log_y'/'log_x' > daq.BooleanSwitch(id='{}-bool', on=False)
+        # Won't work: error_x..., animation_frame/animation_group, category_orders, labels, etc.
+    ])
 
 
-class Tab2(TabBase):
-    """Tab2 properties."""
+class Tab4(TabBase):
+    """Tab4 properties."""
+    title = 'Ternary'
+    data = px.data.election()
+    funcMap = OrderedDict([
+        ('scatter_ternary', px.scatter_ternary),
+        ('line_ternary', px.line_ternary),
+    ])
+    dims = ('a', 'b', 'c', 'color', 'size')
+
+
+class Tab5(TabBase):
+    """Tab5 properties."""
+    title = 'Polar'
+    data = px.data.wind()
+    funcMap = OrderedDict([
+        ('scatter_polar', px.scatter_polar),
+        ('line_polar', px.line_polar),  # (line_close=True)
+    ])
+    dims = ('r', 'theta', 'color')
+
+
+class Tab6(TabBase):
+    """Tab6 properties."""
     title = 'Color Swatches'
     takesArgs = False
     funcMap = OrderedDict([
@@ -91,7 +104,7 @@ class Tab2(TabBase):
     ])
 
 
-TABS = [Tab1(), Tab2()]
+TABS = [Tab1(),  Tab4(), Tab5(), Tab6()]
 TAB_LOOKUP = {_tab.title: _tab for _tab in TABS}
 
 # ======================================================================================================================
@@ -107,7 +120,7 @@ app.layout = html.Div([
     dcc.Tabs(id='tabs-select', value=TABS[0].title, children=[
         dcc.Tab(label=_tab.title, value=_tab.title) for _tab in TABS
     ], style={'margin-bottom': '15px'}),
-    html.Div(id='tabs-content')
+    html.Div(id='tabs-content'),
 ])
 TAB_MAP = {
     _tab.title: html.Div([
@@ -121,7 +134,11 @@ TAB_MAP = {
         ] + [
             html.P([
                 _d + ':', dcc.Dropdown(id='{}-{}'.format(_tab.title, _d), options=_tab.colOpts)
-            ]) for idx, _d in enumerate(_tab.dims)
+            ]) for _d in _tab.dims
+        ] + [
+            html.P([
+                _d + ':', dcc.Dropdown(id='{}-{}'.format(_tab.title, _d), options=[ddOpts(_l, _l) for _l in _list])
+            ]) for _d, _list in _tab.dimsDict.items()
         ], style={'width': '25%', 'float': 'left'},
         ),
         MinGraph(id='{}-graph'.format(_tab.title), style={'width': '75%', 'display': 'inline-block'}),
@@ -140,17 +157,26 @@ def renderTabs(tabName):
 for _tab in TABS:
     inputs = [Input('tabs-select', 'value'), Input('{}-func'.format(_tab.title), 'value')]
     inputs.extend([Input('{}-{}'.format(_tab.title, _d), 'value') for _d in _tab.dims])
+    inputs.extend([Input('{}-{}'.format(_tab.title, _k), 'value') for _k in _tab.dimsDict.keys()])
 
-    @app.callback(Output('{}-graph'.format(_tab.title), 'figure'), inputs)
+    @app.callback(
+        Output('{}-graph'.format(_tab.title), 'figure'),
+        inputs,
+    )
     def renderFigure(tabName, nameFunc, *args):
-        """Create the express figure."""
-        tab = TAB_LOOKUP[tabName]  # Read tab class from input
-        # Suppress errors when switching tabs
-        if nameFunc is None or nameFunc not in tab.funcMap:
+        """Create the figure."""
+        # Check if the trigger event is a tab change. If so, return an empty chart
+        triggeredIDs = [_t['prop_id'] for _t in dash.callback_context.triggered]
+        ic(triggeredIDs)
+        if 'tabs-select.value' in triggeredIDs:
             return {}
-        # Return the figure based on the tab settings
+        # Otherwise, parse the arguments to generate a new plot
+        tab = TAB_LOOKUP[tabName]
         if tab.takesArgs:
-            kwargs = dict(zip(tab.dims, args))
+            keys = list(tab.dims) + list(tab.dimsDict.keys())
+            kwargs = OrderedDict(zip(keys, args))
+            ic(kwargs)
+            ic(tab.data.head(2))
             return tab.funcMap[nameFunc](tab.data, height=650, **kwargs)
         return tab.funcMap[nameFunc]()
 
