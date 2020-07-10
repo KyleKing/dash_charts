8fed5681d08943a73b6ef2a0b2cbd20fbe12f330
Kyle King <KyleKing@users.noreply.github.com>
2020-07-10 19:35:10 -0400

    Chg: cleanup px app

--- dash_charts/app_px.py
+++ dash_charts/app_px.py
@@ -30,91 +30,22 @@ from .utils_fig import min_graph
 
 # ------------------------------------------------------------------------------------------------
 
-# # TODO: Show raw data in <pre> block or table in each tab. Maybe show all the possible datasets
-# px.data.election_geojson()  # Dict
-# px.data.carshare().head()
-# px.data.election().head()
-# px.data.gapminder().head()
-# px.data.iris().head()
-# px.data.tips().head()
-# px.data.wind().head()
-
-# All Chart - TODO: Make sure all these charts are in example
-# scatter([data_frame, x, y, color, symbol, …])
-#    In a scatter plot, each row of data_frame is represented by a symbol
-# scatter_3d([data_frame, x, y, z, color, …])
-#    In a 3D scatter plot, each row of data_frame is represented by a
-# scatter_polar([data_frame, r, theta, color, …])
-#    In a polar scatter plot, each row of data_frame is represented by a
-# scatter_ternary([data_frame, a, b, c, …])
-#    In a ternary scatter plot, each row of data_frame is represented by a
-# scatter_mapbox([data_frame, lat, lon, …])
-#    In a Mapbox scatter plot, each row of data_frame is represented by a
-# scatter_geo([data_frame, lat, lon, …])
-#    In a geographic scatter plot, each row of data_frame is represented
-#
-# line([data_frame, x, y, line_group, color, …])
-#    In a 2D line plot, each row of data_frame is represented as vertex of
-# line_3d([data_frame, x, y, z, color, …])
-#    In a 3D line plot, each row of data_frame is represented as vertex of
-# line_polar([data_frame, r, theta, color, …])
-#    In a polar line plot, each row of data_frame is represented as vertex
-# line_ternary([data_frame, a, b, c, color, …])
-#    In a ternary line plot, each row of data_frame is represented as
-# line_mapbox([data_frame, lat, lon, color, …])
-#    In a Mapbox line plot, each row of data_frame is represented as
-# line_geo([data_frame, lat, lon, locations, …])
-#    In a geographic line plot, each row of data_frame is represented as
-#
-# area([data_frame, x, y, line_group, color, …])
-#    In a stacked area plot, each row of data_frame is represented as
-#
-# bar([data_frame, x, y, color, facet_row, …])
-#    In a bar plot, each row of data_frame is represented as a rectangular
-# bar_polar([data_frame, r, theta, color, …])
-#    In a polar bar plot, each row of data_frame is represented as a wedge
-#
-# violin([data_frame, x, y, color, facet_row, …])
-#    In a violin plot, rows of data_frame are grouped together into a
-# box([data_frame, x, y, color, facet_row, …])
-#    In a box plot, rows of data_frame are grouped together into a
-# strip([data_frame, x, y, color, facet_row, …])
-#    In a strip plot each row of data_frame is represented as a jittered
-# histogram([data_frame, x, y, color, …])
-#    In a histogram, rows of data_frame are grouped together into a
-# pie([data_frame, names, values, color, …])
-#    In a pie plot, each row of data_frame is represented as a sector of a
-# treemap([data_frame, names, values, …])
-#    A treemap plot represents hierarchial data as nested rectangular
-# sunburst([data_frame, names, values, …])
-#    A sunburst plot represents hierarchial data as sectors laid out over
-# funnel([data_frame, x, y, color, facet_row, …])
-#    In a funnel plot, each row of data_frame is represented as a
-# funnel_area([data_frame, names, values, …])
-#    In a funnel area plot, each row of data_frame is represented as a
+# Other charts that could be useful (but won't work with simple dropdowns)
 # scatter_matrix([data_frame, dimensions, …])
 #    In a scatter plot matrix (or SPLOM), each row of data_frame is
-#
+#    https://plotly.com/python/splom/
 # parallel_coordinates([data_frame, …])
 #    In a parallel coordinates plot, each row of data_frame is represented
+#    https://plotly.com/python-api-reference/generated/plotly.express.parallel_coordinates.html
 # parallel_categories([data_frame, …])
 #    In a parallel categories (or parallel sets) plot, each row of
-#
-# choropleth([data_frame, lat, lon, …])
-#    In a choropleth map, each row of data_frame is represented by a
-# choropleth_mapbox([data_frame, geojson, …])
-#    In a Mapbox choropleth map, each row of data_frame is represented by a
-#
-# density_contour([data_frame, x, y, z, …])
-#    In a density contour plot, rows of data_frame are grouped together
+#    https://plotly.com/python-api-reference/generated/plotly.express.parallel_categories.html
 # density_heatmap([data_frame, x, y, z, …])
 #    In a density heatmap, rows of data_frame are grouped together into
-# density_mapbox([data_frame, lat, lon, z, …])
-#    In a Mapbox density map, each row of data_frame contributes to the intensity of
-#
+#    https://plotly.com/python-api-reference/generated/plotly.express.density_heatmap.html
 # imshow(img[, zmin, zmax, origin, …])
 #    Display an image, i.e.
-
+#    https://plotly.com/python/imshow/
 
 # ======================================================================================================================
 # Create classes to manage tabs state. Easy to scale up or down
@@ -264,9 +195,8 @@ class TabBase(AppBase):  # noqa: H601
 class TabTip(TabBase):  # noqa: H601
     """TabTip properties."""
 
-    name = 'Tip Data (Iris)'  # FIXME: tips has non-numeric types
-    data = px.data.iris()  # https://plotly.com/python/plotly-express/
-    # data = px.data.tips()
+    name = 'Tip Data'
+    data = px.data.tips()
     func_map = OrderedDict([
         ('scatter', px.scatter),
         ('density_contour', px.density_contour),
@@ -276,14 +206,12 @@ class TabTip(TabBase):  # noqa: H601
         ('marginal_x', ('histogram', 'rag', 'violin', 'box')),
         ('marginal_y', ('histogram', 'rag', 'violin', 'box')),
         ('trendline', ('ols', 'lowess')),
-        # Consider: 'log_y'/'log_x' > daq.BooleanSwitch(id='{}-bool', on=False)
-        # Won't work: error_x..., animation_frame/animation_group, category_orders, labels, etc.
     ])
     default_dim_name = {
-        'x': 'sepal_width',
-        'y': 'sepal_length',
+        'x': 'total_bill',
+        'y': 'tip',
+        'color': 'smoker',
     }
-    """Lookup for dim:column name to use as default in dropdown."""
 
 
 class TabIris(TabBase):  # noqa: H601
@@ -293,13 +221,19 @@ class TabIris(TabBase):  # noqa: H601
     data = px.data.iris()
     func_map = OrderedDict([
         ('histogram', px.histogram),
+        ('parallel_coordinates', px.parallel_coordinates),
+        ('box', px.box),
         ('density_contour', px.density_contour),
         ('strip', px.strip),
-        ('box', px.box),
+        ('bar', px.bar),
         ('violin', px.violin),
         ('scatter', px.scatter),
     ])
-    dims = ('x', 'y', 'color', 'facet_col', 'facet_row')
+    dims = ('x', 'y', 'color', 'facet_row', 'facet_col')
+    default_dim_name = {
+        'x': 'sepal_width',
+        'color': 'species',
+    }
 
 
 class TabGapminder(TabBase):  # noqa: H601
@@ -309,8 +243,17 @@ class TabGapminder(TabBase):  # noqa: H601
     data = px.data.gapminder()
     func_map = OrderedDict([
         ('area', px.area),
+        ('line', px.line),
+        ('treemap', px.treemap),
+        ('sunburst', px.sunburst),
     ])
     dims = ('x', 'y', 'color', 'line_group', 'facet_row', 'facet_col')
+    default_dim_name = {
+        'x': 'year',
+        'y': 'pop',
+        'color': 'continent',
+        'line_group': 'country',
+    }
 
 
 class TabTernary(TabBase):  # noqa: H601
@@ -322,19 +265,34 @@ class TabTernary(TabBase):  # noqa: H601
         ('scatter_ternary', px.scatter_ternary),
         ('line_ternary', px.line_ternary),
     ])
-    dims = ('a', 'b', 'c', 'color', 'size')
+    dims = ('a', 'b', 'c', 'color', 'size', 'hover_name')
+    default_dim_name = {
+        'a': 'Joly',
+        'b': 'Coderre',
+        'c': 'Bergeron',
+        'color': 'winner',
+        'size': 'total',
+        'hover_name': 'district',
+    }
 
 
-class TabPolar(TabBase):  # noqa: H601
-    """TabPolar properties."""
+class TabWind(TabBase):  # noqa: H601
+    """TabWind properties."""
 
-    name = 'Polar'
+    name = 'Wind'
     data = px.data.wind()
     func_map = OrderedDict([
         ('scatter_polar', px.scatter_polar),
         ('line_polar', px.line_polar),  # (line_close=True)
+        ('bar_polar', px.bar_polar),
     ])
     dims = ('r', 'theta', 'color')
+    default_dim_name = {
+        'r': 'frequency',
+        'theta': 'direction',
+        'color': 'strength',
+        'symbol': 'strength',
+    }
 
 
 class TabColor(TabBase):  # noqa: H601
@@ -351,6 +309,10 @@ class TabColor(TabBase):  # noqa: H601
         ('colors.cmocean', px.colors.cmocean.swatches),
         ('colors.carto', px.colors.carto.swatches),
     ])
+    default_dim_name = {
+        'x': 'sepal_width',
+        'y': 'sepal_length',
+    }
 
 # ======================================================================================================================
 # Create class for application to control manage variable scopes
@@ -382,7 +344,7 @@ class InteractivePXApp(FullScreenAppWithTabs):  # noqa: H601
             TabIris(app=self.app),
             TabGapminder(app=self.app),
             TabTernary(app=self.app),
-            TabPolar(app=self.app),
+            TabWind(app=self.app),
             TabColor(app=self.app),
         ]
 
