0a227fb2feb7514f9733e680fc5d3c4655e7c2db
Kyle King <KyleKing@users.noreply.github.com>
2020-07-12 17:59:52 -0400

    Chg: create legendgroups to toggle projects

--- dash_charts/gantt.py
+++ dash_charts/gantt.py
@@ -1,54 +1,52 @@
 """Gantt Chart.
 
-- N/A: https://plotly.com/python/gantt/
-- N/A: https://plotly.com/python/horizontal-bar-charts/#basic-horizontal-bar-chart
+# TODO:
 
-- Shapes: https://plotly.com/python/shapes/
-- DT Axis: https://stackoverflow.com/a/51231209/3219667
-- Hmm: https://stackoverflow.com/questions/58493254/how-to-add-more-than-one-shape-with-loop-in-plotly
+- Refactor and cleanup documentation
+- Fix hover on deployment
+- Review below examples
+- Fix color generation for multiple projects
 
-Examples
+# Inspiration
 
 - https://github.com/alampros/Gantt-Chart/blob/master/gantt-chart-d3.js
 - http://guypursey.com/blog/201605302300-d3-timescale-visualisation
 - https://shybovycha.github.io/2017/04/09/gantt-chart-with-d3.html
 - https://github.com/dk8996/Gantt-Chart
 
-# TODO: Gantt Tasks
-
-- Fix hover text to shape
-- Show full hover text information on hover (avoid cropping)
-- Sort, then draw shapes accordingly. Determine the x/y range
-- Show label on YAxis? (Hide numeric values)
-- Add bolded headers with horizontal bar for all subtasks
-- Add milestones with scatter point
-
 # Other Ideas
 
+- Consider how to display task dependencies
 - Consider adding dependencies (end date = start date) where vertical line could connect them?
 - Consider relative dates
-- Consider assigning resources with some sort of indicator
-- If items are filtered from the view, compress the YAxis (see axis-breaks in px.scatter?)
-- Potentially show slider chart underneath for adjustable time view (probably just use the default zoom)
+- -Consider assigning resources with some sort of indicator-
+
+# Removed COde
+
+```py
+dates = sorted(set(filter(None, df_raw['start'].to_list() + df_raw['end'].to_list())))
+self.axis_range = {'x': [dates[0], dates[-1]]}
+```
 
 """
 
 from datetime import datetime
-import plotly.graph_objects as go
-from icecream import ic
 
-from .dash_helpers import validate
-from .utils_fig import CustomChart, check_raw_data
+import plotly.graph_objects as go
+
+from .utils_fig import CustomChart
 
 
 def get_unix(str_ts, date_format):
+
     return datetime.strptime(str_ts, date_format).timestamp()
 
 
 class GanttChart(CustomChart):  # noqa: H601
     """Gantt Chart: event and resource timeline."""
 
-    # # See `wesanderson::wes_palette` for R, such as Darjeeling1
+    date_format = '%Y-%m-%d'
+    """Date format for bar chart."""
 
     def create_traces(self, df_raw):
         """Return traces for plotly chart.
@@ -59,69 +57,95 @@ class GanttChart(CustomChart):  # noqa: H601
         Returns:
             list: Dash chart traces
 
-        Raises:
-            RuntimeError: if the `df_raw` is missing any necessary columns
-
         """
-        events = []
+        tasks = []
         self.shapes = []
         self.annotations = []
-        # FIXME: Replace start==None, with end value! Will fix sort and simplify some logic below
+        # Sort dataframe and create color lookup
         df_raw = (df_raw
                   .sort_values(by=['category', 'start'], ascending=False)
                   .sort_values(by=['end'], ascending=False)
                   .reset_index(drop=True))
-        dates = sorted(set(filter(None, df_raw['start'].to_list() + df_raw['end'].to_list())))
-        self.axis_range = {'x': [dates[0], dates[-1]]}
-        for event in df_raw.itertuples():
-            start = event.start if event.start else event.end
-            date_range = (f'{event.start}-' if event.start else '') + event.end
-            y_pos = event.Index
-            events.append(go.Scatter(
-                # fillcolor=self.colors[event.category],
-                # legendgroup=self.colors[event.category],
-                # marker={'color': self.colors[event.category]},
+        categories = set(df_raw['category'])
+        colors = ('#D5D5D3', '#CEAB07')
+        self.color_lookup = {cat: colors[idx] for idx, cat in enumerate(categories)}
+        groups = []
+        # Add all tasks as horizontal bars with progress overlay and text label
+        for task in df_raw.itertuples():
+            color = self.color_lookup[task.category]
+            start = task.start if task.start else task.end
+            # FIXME: make this two lines and format as XXMMMYYYY in GDP format
+            date_range = (f'{task.start} to ' if task.start else '') + task.end
+            y_pos = task.Index
+            is_first = task.category not in groups
+            groups.append(task.category)
+            scatter_kwargs = dict(
+                fill='toself',
+                fillcolor=color,
+                hoverlabel={'bgcolor': 'white', 'font_size': 12, 'namelength': 0},
+                legendgroup=color,
+                line={'width': 1},
+                marker={'color': color},
                 mode='lines',
-                text=f'<b>{event.category}</b><br>{event.label} ({int(event.progress * 100)}%)<br>{date_range}',
-                x=[start, event.end, event.end, start, start],
+                showlegend=is_first,
+                text=f'<b>{task.category}</b><br>{task.label} ({int(task.progress * 100)}%)<br>{date_range}',
+                x=[start, task.end, task.end, start, start],
                 y=[y_pos, y_pos, y_pos - 1, y_pos - 1, y_pos],
-                showlegend=False,  # FIXME: On first use of the legend group, give name set this to True
-            ))
+            )
+            if is_first:
+                scatter_kwargs['name'] = task.category
+            tasks.append(go.Scatter(**scatter_kwargs))
             # Add progress overlay and task label
-            if event.progress > 0:
-                self._add_progress_shape(event, y_pos)
-            self._add_annotation(event, y_pos)
-        return events
+            if task.progress > 0:
+                tasks.append(self._add_progress_shape(task, y_pos))
+            tasks.append(self._add_annotation(task, y_pos))
+        return tasks
 
-    def _add_annotation(self, event, y_pos):
-        self.annotations.append({
-            'showarrow': False,
-            'text': event.label,
-            'align': 'right',
-            'x': event.end,
-            'xanchor': 'left',
-            'y': y_pos - 0.5,
-            'yanchor': 'middle',
-        })
+    def _add_annotation(self, task, y_pos):
+        """Add task progress to `self.annotations` at y_pos.
 
-    def _add_progress_shape(self, event, y_pos):
-        """Add white overlay to indicate task progress."""
-        start = event.start if event.start else event.end
+        Args:
+            task: row tuple from df_raw with: `(category, label, start, end, progress)`
+            y_pos: top y-coordinate of task
+
+        """
+        return go.Scatter(
+            hoverinfo='skip',
+            legendgroup=self.color_lookup[task.category],
+            mode='text',
+            showlegend=False,
+            text=task.label,
+            textposition='middle left',
+            x=[task.end],
+            y=[y_pos - 0.5],
+        )
+
+    def _add_progress_shape(self, task, y_pos):
+        """Add semi-transparent white overlay `self.shapes` to indicate task progress.
+
+        Args:
+            task: row tuple from df_raw with: `(category, label, start, end, progress)`
+            y_pos: top y-coordinate of task
+
+        """
+        start = task.start if task.start else task.end
         unix_start = get_unix(start, self.date_format)
-        unix_progress = (get_unix(event.end, self.date_format) - unix_start) * event.progress + unix_start
-        self.shapes.append(go.layout.Shape(
+        unix_progress = (get_unix(task.end, self.date_format) - unix_start) * task.progress + unix_start
+        end = datetime.fromtimestamp(unix_progress).strftime(self.date_format)
+        return go.Scatter(
+            fill='toself',
             fillcolor='white',
-            layer='above',
+            hoverinfo='skip',
+            legendgroup=self.color_lookup[task.category],
             line_width=0,
+            line={'width': 1},
+            marker={'color': 'white'},
+            mode='lines',
             opacity=0.5,
-            type='rect',
-            x0=start,
-            x1=datetime.fromtimestamp(unix_progress).strftime(self.date_format),
-            xref='x',
-            y0=y_pos,
-            y1=y_pos - 1,
-            yref='y',
-        ))
+            showlegend=False,
+            x=[start, end, end, start, start],
+            y=[y_pos, y_pos, y_pos - 1, y_pos - 1, y_pos],
+        )
 
     def create_layout(self):
         """Extend the standard layout.
