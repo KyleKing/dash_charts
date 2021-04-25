61c91e596546c038af3e06ec15d59e05665fa913
Kyle King <KyleKing@users.noreply.github.com>
2021-04-25 18:09:23 -0400

    docs: merge rest of calcipy_template

---
+++ README.md
@@ -0,0 +1,5 @@
+# dash_charts
+
+Python package for Plotly/Dash apps with support for multi-page, modules, and new charts such as Pareto with an Object Orient Approach
+
+Full documentation, here: [./docs](./docs)
--- tests/configuration.py
+++ tests/configuration.py
@@ -2,11 +2,28 @@
 
 from pathlib import Path
 
-TEST_DIR = Path(__file__).resolve().parent
+from calcipy import __pkg_name__
+from calcipy.log_helpers import build_logger_config
+from loguru import logger
+
+TEST_DIR: Path = Path(__file__).resolve().parent
 """Path to the `test` directory that contains this file and all other tests."""
 
-TEST_DATA_DIR = TEST_DIR / 'data'
+TEST_DATA_DIR: Path = TEST_DIR / 'data'
 """Path to subdirectory with test data within the Test Directory."""
+
+# PLANNED: Move all of this into a function! (and/or task?) {Duplicate of dodo.py}
+
+logger.enable(__pkg_name__)  # This will enable output from calcipy, which is off by default
+# See an example of toggling loguru at: https://github.com/KyleKing/calcipy/tree/examples/loguru-toggle
+
+path_project = Path(__file__).resolve().parent
+log_config = build_logger_config(path_project, production=False)
+logger.configure(**log_config)
+logger.info(
+    'Started logging to {path_project}/.logs with {log_config}', path_project=path_project,
+    log_config=log_config,
+)
 
 
 # PLANNED: Output the test name and other information to the test.log file. Currently only used in `no_log_errors`
