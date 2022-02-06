d4c93306df8de7e7e511c116cfafd4dfb0dbed12
Kyle King <KyleKing@users.noreply.github.com>
2022-02-06 12:34:41 -0500

    build: drop CalVer

--- dash_charts/__init__.py
+++ dash_charts/__init__.py
@@ -5,7 +5,7 @@ import warnings
 from beartype.roar import BeartypeDecorHintPepDeprecationWarning
 from loguru import logger
 
-__version__ = '2021.0.1.0a0'
+__version__ = '0.1.0a0'
 __pkg_name__ = 'dash_charts'
 
 logger.disable(__pkg_name__)
